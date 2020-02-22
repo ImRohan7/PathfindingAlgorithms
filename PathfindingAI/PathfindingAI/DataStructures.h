a1bd3401e486d63f495f03522cc69dcd5fff242a
Rohan <rohanpatel1899@gmail.com>
2020-02-22 13:16:54 -0700

    Djkstra init

---
+++ PathfindingAI/PathfindingAI/DataStructures.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <vector>
+#include <queue>
+#include <array>
+
+struct GridLocation {
+	int x, y;
+};
+
+// Priority Queue
+template<typename T, typename priority_t>
+struct PriorityQueue {
+	typedef std::pair<priority_t, T> PQElement;
+	std::priority_queue<PQElement, std::vector<PQElement>,
+		std::greater<PQElement>> elements;
+
+	inline bool empty() const {
+		return elements.empty();
+	}
+
+	inline void put(T item, priority_t priority) {
+		elements.emplace(priority, item);
+	}
+
+	T get() {
+		T best_item = elements.top().second;
+		elements.pop();
+		return best_item;
+	}
+};
+
+// Graph
+// 4 X 4
+// GridLocation <-> array<neighbours> <-> weight
+
